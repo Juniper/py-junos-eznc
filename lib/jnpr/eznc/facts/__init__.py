a0ccc6c5ee4cc619d829401f54e429d7e24a8554
Jeremy Schulman <jschulman@juniper.net>
2013-10-27 12:03:40 -0400

    facts

--- lib/jnpr/eznc/facts/__init__.py
+++ lib/jnpr/eznc/facts/__init__.py
@@ -1,9 +1,11 @@
 from .chassis import chassis
 from .routing_engines import routing_engines
+from .personality import personality
 
 FACT_LIST = [
   chassis,
-  routing_engines
+  routing_engines,
+  personality
 ]
 
 __all__ = ['FACT_LIST']
---
+++ lib/jnpr/eznc/facts/personality.py
@@ -0,0 +1,25 @@
+import re
+
+def personality( junos, facts ):
+  
+  model = facts['hardwaremodel']
+  examine = model if model != 'Virtual Chassis' else facts['RE0']['model']
+        
+  if re.match("^(EX)|(QFX)", examine):
+    persona = 'SWITCH'
+  elif re.match("^MX", examine):
+    persona = 'MX'
+  elif re.match("^vMX", examine):
+    facts['virtual'] = true
+    persona = 'MX'
+  elif re.match("SRX(\d){3}", examine):
+    persona = 'SRX_BRANCH'
+  elif re.match("junosv-firefly", examine, re.IGNORECASE):
+    facts['virtual'] = True
+    persona = 'SRX_BRANCH'
+  elif re.match("SRX(\d){4}", examine):
+    persona = 'SRX_HIGHEND'
+  else:
+    raise RuntimeError("Unknown device persona: %s" % examine)
+  
+  facts['personality'] = persona
