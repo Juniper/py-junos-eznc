b0eb55c730e608ea6190209288e3d32381c92339
Jeremy Schulman <jschulman@juniper.net>
2013-11-14 08:36:05 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "junos-eznc",
+    namespace_packages = ['jnpr'],
     version = "0.0.1",
     author = "Jeremy Schulman",
     author_email = "jschulman@juniper.net",
