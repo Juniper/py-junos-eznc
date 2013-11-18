4772fa300bd925bfb2043c3df824f7a1c2093e4b
Jeremy Schulman <jschulman@juniper.net>
2013-11-17 20:37:33 -0500

    updates

--- lib/jnpr/eznc/runstat/phyport.py
+++ lib/jnpr/eznc/runstat/phyport.py
@@ -1,5 +1,7 @@
 from . import RunstatTable, RunstatView
 
+__all__ = ['PhyPortTable','PhyPortView','PhyPortView2']
+
 class PhyPortTable( RunstatTable ):
   GET_RPC = 'get_interface_information'
   GET_ARGS = dict(media=True)
@@ -25,3 +27,15 @@ class PhyPortView(RunstatView):
     'tx_bytes','tx_packets'
   ]
 
+class PhyPortView2( PhyPortView ):
+  def __init__(self,as_xml):
+    PhyPortView.__init__(self,as_xml)
+
+    new_fields = {
+      'rx_pps' : 'traffic-statistics/input-pps',
+      'tx_pps' : 'traffic-statistics/output-pps'
+    }
+
+    with self.extend() as more:
+      more.field_xpath = dict(new_fields)
+      more.field_as_int = new_fields.keys()
--- lib/jnpr/eznc/runstat/view.py
+++ lib/jnpr/eznc/runstat/view.py
@@ -1,11 +1,23 @@
+from contextlib import contextmanager
+from copy import deepcopy
 from lxml import etree
 
 class RunstatView(object):
+  """
+  RunstatView is the base-class that makes extracting values from XML 
+  data appear as objects with attributes.
+  """
+
   NAME_XPATH = 'name'
   FIELD_AS_INT = []
   FIELD_AS = {}
 
   def __init__(self,as_xml):
+    """
+    :as_xml:
+      this should be an lxml etree Elemenet object.  This 
+      constructor also accepts a list with a single item/XML
+    """
     # if as_xml is passed as a list, make sure it only has
     # a single item, common response from an xpath search
     if isinstance(as_xml,list):
@@ -20,27 +32,34 @@ class RunstatView(object):
 
     self._xml = as_xml
 
+  def __repr__(self):
+    """ returns the name of the View with the associate items name """
+    return "%s(%s)" % (self.__class__.__name__, self.name)
+
   @property 
   def fields(self):
+    """ list of view field names """
     return self.FIELD_XPATH.keys()
 
   def keys(self):
+    """ list of view keys, i.e. field names """
     return self.fields 
 
   def values(self):
+    """ list of view values """
     return [getattr(self,field) for field in self.fields]
 
   def items(self):
+    """ list of tuple(key,value) """
     return zip(self.keys(), self.values())
 
   @property 
   def name(self):
+    """ return the name of view item """
     return self._xml.findtext(self.NAME_XPATH).strip()
 
-  def __repr__(self):
-    return "%s(%s)" % (self.__class__.__name__, self.name)
-
   def __getattr__(self,name):
+    """ retrieve a view field item value """
     xpath = self.FIELD_XPATH.get(name)
     if xpath is None:
       raise ValueError("Unkown field: '%s'" % name)
@@ -57,3 +76,37 @@ class RunstatView(object):
       found = self.FIELD_AS[name](found)
 
     return found
+
+  @contextmanager
+  def extend(self):
+    """ 
+    provide the ability for subclassing objects to extend the
+    definitions of the fields.  this is implemented as a 
+    context manager with the form called from the subclass 
+    constructor:
+
+      with self.extend() as more:
+        more.field_xpath = <dict>
+        more.fied_as_int = <list>   # optinal
+        more.field_as = <dict>      # optional
+
+    """
+    self.FIELD_XPATH = deepcopy(self.__class__.FIELD_XPATH)
+
+    class MoreView(object): pass
+    more = MoreView()
+    yield more
+
+    self.FIELD_XPATH.update(more.field_xpath)
+
+    if hasattr(more,'field_as_int'):
+      self.FIELD_AS_INT = deepcopy(self.__class__.FIELD_AS_INT)
+      self.FIELD_AS_INT += more.field_as_int
+
+    if hasattr(more,'field_as'):
+      self.FIELD_AS = deepcopy(self.__class__.FIELD_AS)
+      self.FIELD_AS.update(more.field_as)
+
+
+
+
