63fe4e9ac7966e98a0ea06bd75527115e22c57cc
Kurt Bales <kwbales@kwbales.net>
2013-11-28 16:49:41 +1100

    Added Basic LLDP Neighbour details. Currently keyed on interfaces

---
+++ lib/jnpr/junos/op/lldp.py
@@ -0,0 +1,8 @@
+from os.path import dirname,join
+from .yaml import loadyaml
+
+# import the definitions from the YAML file in this directory and
+# make them part of this module, yo!
+
+_yml_file = 'lldp.yml'
+globals().update( loadyaml(join(dirname(__file__),_yml_file)) )
