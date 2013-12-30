c0be903a0f98349ef052bc032b8e2697a9058f9f
Jeremy Schulman <jschulman@juniper.net>
2013-12-30 08:59:42 -0500

    updates

--- lib/jnpr/junos/__init__.py
+++ lib/jnpr/junos/__init__.py
@@ -1,8 +1,9 @@
 from .device import Device
 import jxml
 import jxml as JXML  
-import os.path 
+from os.path import dirname as _dirname
 
 __version__ = '0.0.3'
 __date__ = "2013-DEC-31"
-__path__ = os.path.dirname(__file__)
+__path__ = _dirname(__file__)
+
