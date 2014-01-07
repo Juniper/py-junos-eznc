bdfbb53a9a8188e3aaad49326bbd4aee1387c875
Jeremy Schulman <jschulman@juniper.net>
2014-01-06 22:09:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 sys.path.insert(0,'lib')
 from setuptools import setup, find_packages
 
-requirements = [ 'netaddr >= 0.7.10', 'jinja2 >= 2.7.1', 'lxml >= 3.2.4',
+requirements = [ 'netaddr', 'jinja2 >= 2.7.1', 'lxml >= 3.2.4',
                  'scp >= 0.7.0', 'PyYAML >= 3.10' ]
 
 setup(
