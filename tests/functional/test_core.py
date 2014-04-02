27e0d301fcc5c2cc8770df2fa68b7354fae79a71
Rick Sherman <rick@shermdog.com>
2014-04-02 15:02:44 -0500

    Using setUpClass
    May move to setUpModule eventually

---
+++ tests/functional/test_core.py
@@ -0,0 +1,44 @@
+'''
+
+@author: rsherman
+'''
+import unittest
+
+
+class TestCore(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        from jnpr.junos import Device
+        self.dev = Device(host='10.0.0.31', user='rick', password='password123')
+        self.dev.open()
+
+    @classmethod
+    def tearDownClass(self):
+        self.dev.close()
+
+    def test_device_open(self):
+        self.assertEqual(self.dev.connected, True)
+
+    def test_device_facts(self):
+        assert self.dev.facts['hostname'] == 'firefly'
+
+    def test_device_get_timeout(self):
+        assert self.dev.timeout == 30
+
+    def test_device_set_timeout(self):
+        self.dev.timeout = 35
+        assert self.dev.timeout == 35
+
+    def test_device_cli(self):
+        self.assertIn('firefly', self.dev.cli('show version'))
+
+    def test_device_rpc(self):
+        sw = self.dev.rpc.get_software_information()
+        hostname = sw.findtext('host-name')
+        self.assertEqual(hostname, 'firefly')
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'TestCore.testName']
+    unittest.main()
