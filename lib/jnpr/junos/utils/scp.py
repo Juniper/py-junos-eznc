fc163c7d5c95ee380396cec598f8bbb3b297f80f
Nitin Kumar <nitin.nitp@gmail.com>
2014-04-11 12:51:34 +0530

    pep8 changes for utils folder

--- lib/jnpr/junos/utils/scp.py
+++ lib/jnpr/junos/utils/scp.py
@@ -3,48 +3,52 @@ from __future__ import absolute_import
 import paramiko
 from scp import SCPClient
 
+
 class SCP(object):
-  
-  def __init__(self, junos, **scpargs):
-    """
-    constructor that wraps a paramiko 'scp' object.  
-    """
-    self._junos = junos
-    self._scpargs = scpargs
 
-  def open(self, **scpargs):
-    """
-    creates an instance of the scp object and return to caller for use
-    """
-    #@@@ should check for multi-calls to connect to ensure we don't keep opening
-    #@@@ new connections
-    junos = self._junos
-    self._ssh = paramiko.SSHClient()
-    self._ssh.load_system_host_keys()
-    self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    def __init__(self, junos, **scpargs):
+        """
+        constructor that wraps a paramiko 'scp' object.
+        """
+        self._junos = junos
+        self._scpargs = scpargs
 
-    # use junos._hostname since this will be correct if we are going
-    # through a jumphost.
+    def open(self, **scpargs):
+        """
+        creates an instance of the scp object and return to caller for use
+        """
+        #@@@ should check for multi-calls to connect to ensure we don't keep
+        #@@@ opening new connections
+        junos = self._junos
+        self._ssh = paramiko.SSHClient()
+        self._ssh.load_system_host_keys()
+        self._ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    self._ssh.connect( hostname = junos._hostname, 
-      port=(22,int(junos._port))[junos._hostname=='localhost'],
-      username=junos._auth_user,
-      password=junos._auth_password,
-    )    
-    return SCPClient(self._ssh.get_transport(), **scpargs)
+        # use junos._hostname since this will be correct if we are going
+        # through a jumphost.
 
-  def close(self):
-    """
-    closes the ssh/scp connection to the device
-    """
-    self._ssh.close()
+        self._ssh.connect(hostname=junos._hostname,
+                          port=(
+                              22, int(
+                                  junos._port))[
+                              junos._hostname == 'localhost'],
+                          username=junos._auth_user,
+                          password=junos._auth_password,
+                          )
+        return SCPClient(self._ssh.get_transport(), **scpargs)
 
-  ### -------------------------------------------------------------------------
-  ### CONTEXT MANAGER
-  ### -------------------------------------------------------------------------
+    def close(self):
+        """
+        closes the ssh/scp connection to the device
+        """
+        self._ssh.close()
 
-  def __enter__(self):
-    return self.open(**self._scpargs)
+    # -------------------------------------------------------------------------
+    # CONTEXT MANAGER
+    # -------------------------------------------------------------------------
 
-  def __exit__(self,exc_ty,exc_val,exc_tb):
-    self.close()    
+    def __enter__(self):
+        return self.open(**self._scpargs)
+
+    def __exit__(self, exc_ty, exc_val, exc_tb):
+        self.close()
--- lib/jnpr/junos/utils/util.py
+++ lib/jnpr/junos/utils/util.py
@@ -1,35 +1,36 @@
 class Util(object):
-  """ Base class for all utility classes """
-  
-  def __init__(self,dev):
-    self._dev = dev
 
-  def __repr__(self):
-    return "jnpr.junos.utils.%s(%s)" % (self.__class__.__name__, self._dev.hostname)
+    """ Base class for all utility classes """
 
-  ### -------------------------------------------------------------------------
-  ### property: dev
-  ### -------------------------------------------------------------------------
+    def __init__(self, dev):
+        self._dev = dev
 
-  @property
-  def dev(self):
-    """ return the Device device object """
-    return self._dev
+    def __repr__(self):
+        return "jnpr.junos.utils.%s(%s)" % (
+            self.__class__.__name__, self._dev.hostname)
 
-  @dev.setter
-  def dev(self, value):
-    raise RuntimeError("read-only: dev")
+    # -------------------------------------------------------------------------
+    # property: dev
+    # -------------------------------------------------------------------------
 
-  ### -------------------------------------------------------------------------
-  ### property: rpc
-  ### -------------------------------------------------------------------------
+    @property
+    def dev(self):
+        """ return the Device device object """
+        return self._dev
 
-  @property
-  def rpc(self):
-    """ return the Device RPC meta object """
-    return self._dev.rpc
+    @dev.setter
+    def dev(self, value):
+        raise RuntimeError("read-only: dev")
 
-  @rpc.setter
-  def rpc(self, value):
-    raise RuntimeError("read-only: rpc")
-  
+    # -------------------------------------------------------------------------
+    # property: rpc
+    # -------------------------------------------------------------------------
+
+    @property
+    def rpc(self):
+        """ return the Device RPC meta object """
+        return self._dev.rpc
+
+    @rpc.setter
+    def rpc(self, value):
+        raise RuntimeError("read-only: rpc")
