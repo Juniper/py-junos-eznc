e5888138bd4bfc47683c6b917b29c75c5def27f6
Rick Sherman <rick@shermdog.com>
2014-04-15 13:06:05 -0500

    Added unit attr
    Added the unit attr to support flagged test runs.
    Also cleaned up some PEP8

--- tests/unit/facts/test_personality.py
+++ tests/unit/facts/test_personality.py
@@ -2,72 +2,74 @@ __author__ = "Nitin Kumar, Rick Sherman"
 __credits__ = "Jeremy Schulman"
 
 import unittest
+from nose.plugins.attrib import attr
+
 from jnpr.junos import Device
 from jnpr.junos.facts.personality import personality
 
-facts={}
+
+@attr('unit')
 class TestPersonality(unittest.TestCase):
 
     def setUp(self):
+        self.facts = {}
         self.dev = Device(host='1.1.1.1', user='rick', password='password123',
                           gather_facts=False)
 
     def test_virtual_hassis(self):
-        facts['model'] = 'Virtual Chassis'
-        facts['RE0'] = {'model':'QFX5100'}
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'SWITCH')
-
+        self.facts['model'] = 'Virtual Chassis'
+        self.facts['RE0'] = {'model': 'QFX5100'}
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'SWITCH')
 
     def test_m_ex_qfx_series(self):
-        facts['model'] = 'QFX5100'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'SWITCH')
+        self.facts['model'] = 'QFX5100'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'SWITCH')
 
     def test_mx_series(self):
-        facts['model'] = 'MX80'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'MX')
+        self.facts['model'] = 'MX80'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'MX')
 
     def test_vmx_series(self):
-        facts['model'] = 'vMX80'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'MX')
-        self.assertTrue(facts['virtual'])
+        self.facts['model'] = 'vMX80'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'MX')
+        self.assertTrue(self.facts['virtual'])
 
     def test_vjx_series(self):
-        facts['model'] = 'VJX480'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'SRX_BRANCH')
-        self.assertTrue(facts['virtual'])
+        self.facts['model'] = 'VJX480'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'SRX_BRANCH')
+        self.assertTrue(self.facts['virtual'])
 
     def test_m_series(self):
-        facts['model'] = 'M7i'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'M')
+        self.facts['model'] = 'M7i'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'M')
 
     def test_t_series(self):
-        facts['model'] = 'T320'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'T')
+        self.facts['model'] = 'T320'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'T')
 
     def test_ptx_series(self):
-        facts['model'] = 'PTX5000'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'PTX')
+        self.facts['model'] = 'PTX5000'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'PTX')
 
     def test_srx_series(self):
-        facts['model'] = 'SRX210'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'SRX_BRANCH')
+        self.facts['model'] = 'SRX210'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'SRX_BRANCH')
 
     def test_srx_high_series(self):
-        facts['model'] = 'SRX5600'
-        personality(self.dev, facts)
-        self.assertEqual(facts['personality'], 'SRX_HIGHEND')
+        self.facts['model'] = 'SRX5600'
+        personality(self.dev, self.facts)
+        self.assertEqual(self.facts['personality'], 'SRX_HIGHEND')
 
     def test_invalid_series(self):
-        facts['model'] = 'invalid'
+        self.facts['model'] = 'invalid'
         with self.assertRaises(RuntimeError):
-            personality(self.dev, facts)
-
+            personality(self.dev, self.facts)
--- tests/unit/facts/test_switch_style.py
+++ tests/unit/facts/test_switch_style.py
@@ -2,42 +2,45 @@ __author__ = "Nitin Kumar, Rick Sherman"
 __credits__ = "Jeremy Schulman"
 
 import unittest
+from nose.plugins.attrib import attr
+
 from jnpr.junos import Device
 from jnpr.junos.facts.switch_style import switch_style
 
-facts={}
+
+@attr('unit')
 class TestSwitchStyle(unittest.TestCase):
 
     def setUp(self):
+        self.facts = {}
         self.dev = Device(host='1.1.1.1', user='rick', password='password123',
                           gather_facts=False)
 
     def test_switch_style_mx_srx(self):
-        facts['personality'] = 'SRX_HIGHEND'
-        switch_style(self.dev, facts)
-        self.assertEqual(facts['switch_style'], 'BRIDGE_DOMAIN')
+        self.facts['personality'] = 'SRX_HIGHEND'
+        switch_style(self.dev, self.facts)
+        self.assertEqual(self.facts['switch_style'], 'BRIDGE_DOMAIN')
 
     def test_switch_style_model_firefly(self):
-        facts['personality'] = 'SWITCH'
-        facts['model'] = 'FIReFly'
-        switch_style(self.dev, facts)
-        self.assertEqual(facts['switch_style'], 'NONE')
+        self.facts['personality'] = 'SWITCH'
+        self.facts['model'] = 'FIReFly'
+        switch_style(self.dev, self.facts)
+        self.assertEqual(self.facts['switch_style'], 'NONE')
 
     def test_switch_style_model_ex9000(self):
-        facts['personality'] = 'SWITCH'
-        facts['model'] = 'EX9000'
-        switch_style(self.dev, facts)
-        self.assertEqual(facts['switch_style'], 'VLAN_L2NG')
+        self.facts['personality'] = 'SWITCH'
+        self.facts['model'] = 'EX9000'
+        switch_style(self.dev, self.facts)
+        self.assertEqual(self.facts['switch_style'], 'VLAN_L2NG')
 
     def test_switch_style_model_not_specific(self):
-        facts['personality'] = 'SWITCH'
-        facts['model'] = 'abc'
-        switch_style(self.dev, facts)
-        self.assertEqual(facts['switch_style'], 'VLAN')
+        self.facts['personality'] = 'SWITCH'
+        self.facts['model'] = 'abc'
+        switch_style(self.dev, self.facts)
+        self.assertEqual(self.facts['switch_style'], 'VLAN')
 
     def test_switch_style_persona_not_specific(self):
-        facts['personality'] = 'PTX'
-        facts['model'] = 'abc'
-        switch_style(self.dev, facts)
-        self.assertEqual(facts['switch_style'], 'NONE')
-
+        self.facts['personality'] = 'PTX'
+        self.facts['model'] = 'abc'
+        switch_style(self.dev, self.facts)
+        self.assertEqual(self.facts['switch_style'], 'NONE')
