b271dd42e5efb03c7a5c813a31b731f74c338b0b
Nitin Kumar <nitin.nitp@gmail.com>
2014-04-16 14:27:50 +0530

    Added unit tests for swver.py

--- tests/unit/facts/test_swver.py
+++ tests/unit/facts/test_swver.py
@@ -14,6 +14,37 @@ from ncclient.transport import SSHSession
 
 
 @attr('unit')
+class TestVersionInfo(unittest.TestCase):
+
+    def test_version_info_after_type_len_else(self):
+        self.assertIsNone(version_info('12.1X46-D10').build)
+
+    def test_version_info_constructor_else_exception(self):
+        self.assertEqual(version_info('11.4R7').build, '7')
+
+    def test_version_info_repr(self):
+        self.assertEqual(repr(version_info('11.4R7.5')), \
+        'junos.version_info(major=(11, 4), type=R, minor=7, build=5)')
+
+    def test_version_info_lt(self):
+        self.assertTrue(version_info('13.3-20131120')<(14, 1))
+
+    def test_version_info_lt_eq(self):
+        self.assertTrue(version_info('13.3-20131120')<=(14, 1))
+
+    def test_version_info_gt(self):
+        self.assertTrue(version_info('13.3-20131120')>(12, 1))
+
+    def test_version_info_gt_eq(self):
+        self.assertTrue(version_info('13.3-20131120')>=(12, 1))
+
+    def test_version_info_eq(self):
+        self.assertTrue(version_info('13.3-20131120')==(13, 3))
+
+    def test_version_info_not_eq(self):
+        self.assertTrue(version_info('13.3-20131120')!=(15, 3))
+
+@attr('unit')
 class TestSrxCluster(unittest.TestCase):
 
     @patch('ncclient.manager.connect')
@@ -30,6 +61,23 @@ class TestSrxCluster(unittest.TestCase):
         self.facts['master'] = 'RE0'
         software_version(self.dev, self.facts)
         self.assertEqual(self.facts['version'], '12.3R6.6')
+
+    @patch('jnpr.junos.Device.execute')
+    def test_swver_master_none(self, mock_execute):
+        mock_execute.side_effect = self._mock_manager
+        self.facts['master'] = None
+        software_version(self.dev, self.facts)
+        self.assertEqual(self.facts['version'], '12.3R6.6')
+
+
+    @patch('jnpr.junos.Device.execute')
+    @patch('jnpr.junos.facts.swver.re.findall')
+    def test_swver_exception_handling(self,  mock_re_findall, mock_execute):
+        mock_execute.side_effect = self._mock_manager
+        mock_re_findall.side_effect = IndexError
+        self.facts['master'] = 'RE0'
+        software_version(self.dev, self.facts)
+        self.assertEqual(self.facts['version'], '0.0I0.0')
 
     def _read_file(self, fname):
         from ncclient.xml_ import NCElement
