cf82c3e93bce69627099e09a69d08d2ff5a1713c
Nitin Kumar <nitin.nitp@gmail.com>
2014-05-06 13:31:29 +0530

    code coverage 100% for exception.py and rpcmeta.py

--- tests/unit/test_exception.py
+++ tests/unit/test_exception.py
@@ -3,7 +3,8 @@ __credits__ = "Jeremy Schulman"
 
 import unittest
 from nose.plugins.attrib import attr
-from jnpr.junos.exception import RpcError, CommitError
+from jnpr.junos.exception import RpcError, CommitError, ConnectError
+from jnpr.junos import Device
 from lxml import etree
 
 
@@ -17,7 +18,16 @@ class Test_RpcError(unittest.TestCase):
         self.assertEqual(obj.__repr__(), '<root>\n  <a>test</a>\n</root>\n')
 
     def test_rpcerror_jxml_check(self):
-        #this test is intended to hit jxml code
+        # this test is intended to hit jxml code
         rsp = etree.XML('<rpc-reply><a>test</a></rpc-reply>')
         obj = CommitError(rsp=rsp)
         self.assertEqual(type(obj.rpc_error), dict)
+
+    def test_ConnectError(self):
+        self.dev = Device(host='1.1.1.1', user='rick', password='password123',
+                          gather_facts=False)
+        obj = ConnectError(self.dev)
+        self.assertEqual(obj.user, 'rick')
+        self.assertEqual(obj.host, '1.1.1.1')
+        self.assertEqual(obj.port, 830)
+        self.assertEqual(repr(obj), 'ConnectError(1.1.1.1)')
