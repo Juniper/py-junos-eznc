63cccb08d4799dd2d5ae9f9ca1db4b855a9c94b8
Rick Sherman <rick@shermdog.com>
2014-05-09 15:39:52 -0500

    Added coverage to Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ sys.path.insert(0, os.path.abspath('../lib'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx.ext.coverage',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
