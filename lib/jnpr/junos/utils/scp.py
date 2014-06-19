eb3fcf82f3c699e965048aad59c5b1b6bdc817b9
Jeremy Schulman <nwkautomaniac@gmail.com>
2014-06-19 09:42:00 -0400

    sphinxdoc updates, add to config file to show __init__

--- lib/jnpr/junos/utils/scp.py
+++ lib/jnpr/junos/utils/scp.py
@@ -3,19 +3,46 @@ from __future__ import absolute_import
 import paramiko
 from scp import SCPClient
 
+"""
+Secure Copy Utility
+"""
 
 class SCP(object):
+    """
+    The SCP utility is used to conjunction with :class:`jnpr.junos.utils.sw.SW`
+    when transferring the Junos image to the device.  The :class:`SCP` can be
+    used for other secure-copy use-cases as well; it is implemented to support
+    the python *context-manager* pattern.  For example::
 
+        from jnpr.junos.utils.scp import SCP
+
+        with SCP( dev, progress=_scp_progress ) as scp:
+            scp.put( package, remote_path )
+
+    """
     def __init__(self, junos, **scpargs):
         """
-        constructor that wraps a paramiko 'scp' object.
+        Constructor that wraps :py:mod:`paramiko` and :py:mod:`scp` related objects.
+
+        :param Device junos: the Device object
+        :param kvargs scpargs: any additional args to be passed to paramiko SCP 
         """
         self._junos = junos
         self._scpargs = scpargs
 
     def open(self, **scpargs):
         """
-        creates an instance of the scp object and return to caller for use
+        Creates an instance of the scp object and return to caller for use.
+
+        .. note:: This method uses the same username/password authentication
+                   credentials as used by :class:`jnpr.junos.device.Device`.
+
+        .. warning:: The :class:`jnpr.junos.device.Device` ``ssh_private_key_file`` 
+                     option is currently **not** supported.
+
+        .. todo:: add support for ``ssh_private_key_file``.
+
+        :returns: SCPClient object
         """
         #@@@ should check for multi-calls to connect to ensure we don't keep
         #@@@ opening new connections
@@ -39,7 +66,7 @@ class SCP(object):
 
     def close(self):
         """
-        closes the ssh/scp connection to the device
+        Closes the ssh/scp connection to the device
         """
         self._ssh.close()
 
