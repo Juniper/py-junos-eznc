1fc0c69413bcb7872b418b5425506872d449d13f
Nitin Kumar <nitin.nitp@gmail.com>
2014-12-06 23:14:18 +0530

    more test cases to cover config.py

--- tests/unit/utils/test_config.py
+++ tests/unit/utils/test_config.py
@@ -238,3 +238,40 @@ class TestConfig(unittest.TestCase):
         self.conf.rpc.load_configuration = MagicMock()
         self.assertRaises(ValueError, self.conf.rollback, 51)
         self.assertRaises(ValueError, self.conf.rollback, -1)
+
+    @patch('jnpr.junos.Device.execute')
+    def test_rescue_action_save(self, mock_exec):
+        self.dev.request_save_rescue_configuration = MagicMock()
+        self.assertTrue(self.conf.rescue('save'))
+
+    @patch('jnpr.junos.Device.execute')
+    def test_rescue_action_get_exception(self, mock_exec):
+        self.dev.rpc.get_rescue_information = MagicMock(side_effect=Exception)
+        self.assertIsNone(self.conf.rescue('get'))
+
+    @patch('jnpr.junos.Device.execute')
+    def test_rescue_action_get(self, mock_exec):
+        self.dev.rpc.get_rescue_information = MagicMock()
+        self.dev.rpc.get_rescue_information.return_value = 1
+        self.assertEqual(self.conf.rescue('get', format='xml'), 1)
+
+    @patch('jnpr.junos.Device.execute')
+    def test_rescue_action_delete(self, mock_exec):
+        self.dev.rpc.request_delete_rescue_configuration = MagicMock()
+        self.assertTrue(self.conf.rescue('delete'))
+
+    @patch('jnpr.junos.Device.execute')
+    def test_rescue_action_reload(self, mock_exec):
+        self.dev.rpc.load_configuration = MagicMock()
+        self.dev.rpc.load_configuration.return_value = True
+        self.assertTrue(self.conf.rescue('reload'))
+
+    @patch('jnpr.junos.Device.execute')
+    def test_rescue_action_reload_exception(self, mock_exec):
+        self.dev.rpc.load_configuration = MagicMock(side_effect=Exception)
+        self.assertFalse(self.conf.rescue('reload'))
+
+    @patch('jnpr.junos.Device.execute')
+    def test_rescue_action_unsupported_action(self, mock_exec):
+        with self.assertRaises(ValueError):
+            self.conf.rescue('abc')
