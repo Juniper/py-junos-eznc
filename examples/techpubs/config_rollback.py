4bbe93646146e9a17a5e03126bc4f0d67ceb4231
Rick Sherman <rick@shermdog.com>
2015-07-29 14:47:14 -0500

    Update config_rollback for 1.2

--- examples/techpubs/config_rollback.py
+++ examples/techpubs/config_rollback.py
@@ -1,52 +1,48 @@
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
-from jnpr.junos.exception import *
+from jnpr.junos.exception import ConnectError, LockError, RpcError, CommitError, UnlockError
 
 host = 'dc1a.example.com'
+
 
 def main():
     dev = Device(host=host)
     # open a connection with the device and start a NETCONF session
     try:
         dev.open()
-    except Exception as err:
-        print "Cannot connect to device:", err
+    except ConnectError as err:
+        print "Cannot connect to device: {0}".format(err)
         return
 
-    cu=Config(dev)
+    # Setup config object
+    cu = Config(dev)
 
     # Lock the configuration
     print "Locking the configuration"
     try:
         cu.lock()
-    except LockError:
-        print "Error: Unable to lock configuration"
+    except LockError as err:
+        print "Unable to lock configuration: {0}".format(err)
         dev.close()
         return
-
-
     try:
         print "Rolling back the configuration"
         cu.rollback(rb_id=1)
         print "Committing the configuration"
         cu.commit()
-    except ValueError as err:
-        print err.message
-    except CommitError:
-        print "Error: Unable to commit configuration"
-    except Exception as err:
-        if err.rsp.find('.//ok') is None:
-            rpc_msg = err.rsp.findtext('.//error-message')
-            print "Unable to rollback configuration changes: ", rpc_msg
+    except CommitError as err:
+        print "Error: Unable to commit configuration: {0}".format(err)
+    except RpcError as err:
+        print "Unable to rollback configuration changes: {0}".format(err)
+
     finally:
         print "Unlocking the configuration"
         try:
             cu.unlock()
-        except UnlockError:
-            print "Error: Unable to unlock configuration"
+        except UnlockError as err:
+            print "Unable to unlock configuration: {0}".format(err)
         dev.close()
         return
 
 if __name__ == "__main__":
     main()
-
