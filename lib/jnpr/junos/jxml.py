725773196bb66d76d54b916a40bf7a0cbb685a58
nitin kumar <nitin.nitp@gmail.com>
2016-02-03 16:23:07 +0530

    reverting chnages for python 2.6

--- lib/jnpr/junos/jxml.py
+++ lib/jnpr/junos/jxml.py
@@ -133,7 +133,7 @@ strip_comments_transform = etree.XSLT(strip_xslt_root)
 
 
 def remove_namespaces(xml):
-    for elem in xml.iter():
+    for elem in xml.getiterator():
         if elem.tag is etree.Comment:
             continue
         i = elem.tag.find('}')
