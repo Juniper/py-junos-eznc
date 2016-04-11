b215c4b36b3dd70d783887bd4e48a68e5aaf5381
jainpriyal <jainpriyal168@gmail.com>
2016-04-11 17:49:52 +0530

    removing old netconify

--- lib/jnpr/junos/netconify_bkup/__init__.py
+++
@@ -1,8 +0,0 @@
-from netconify.tty_serial import Serial
-from netconify.tty_telnet import Telnet
-from netconify import constants as C
-
-__version__ = C.version
-__date__ = C.date
-__author__ = C.author
-
--- lib/jnpr/junos/netconify_bkup/constants.py
+++
@@ -1,3 +0,0 @@
-version = "1.0.2"
-date = "2016-Feb-08"
-author = "Jeremy Schulman, @nwkautomaniac"
