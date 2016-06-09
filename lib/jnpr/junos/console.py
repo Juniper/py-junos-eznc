1fc78bf79ab8320b7b0460860260d459185152f3
jainpriyal <jainpriyal168@gmail.com>
2016-06-09 15:59:32 +0530

    committing changes in console file

--- lib/jnpr/junos/console.py
+++ lib/jnpr/junos/console.py
@@ -171,10 +171,13 @@ class Console(object):
         # --------------------
         try:
             self._tty_login()
-        except Exception as err:
-            logger.error("ERROR {0}:{1}\n".format('login', str(err)))
-            logger.error("Complete traceback message: {0}".format(traceback.format_exc()))
+        except RuntimeError as err:
+            logger.error("ERROR:  {0}:{1}\n".format('login', str(err)))
+            logger.error("\nComplete traceback message: {0}".format(traceback.format_exc()))
             raise RuntimeError
+        except Exception as ex:
+            logger.error("Exception occurred: {0} \n".format('login', str(ex)))
+            raise ex
         self.connected = True
         if self.gather_facts is True:
             self._gather_facts()
@@ -189,13 +192,15 @@ class Console(object):
                 self._tty_logout()
             except Exception as err:
                 logger.error("ERROR {0}:{1}\n".format('logout', str(err)))
+                raise err
             self.connected = False
         elif self.connected is True:
             try:
                 self._tty._tty_close()
             except Exception as err:
                 logger.error("ERROR {0}:{1}\n".format('close', str(err)))
-                traceback.print_exc()
+                logger.error("\nComplete traceback message: {0}".format(traceback.format_exc()))
+                raise err
             self.connected = False
 
     # execute rpc calls
@@ -237,25 +242,6 @@ class Console(object):
         self._tty.logout()
 
 
-    def srx_cluster(self, cluster_id, node):
-        """ Enable cluster mode on SRX device"""
-        srx_args = {}
-        srx_args['cluster_id'] = cluster_id
-        srx_args['node'] = node
-        logger.debug("{0}:{1}".format('srx_cluster', 'set device to cluster mode, rebooting'))
-        logger.debug("srx_cluster: Cluster ID: {0}".format(cluster_id))
-        logger.debug("srx_cluster: Node: {0}".format(node))
-        self._tty.nc.enablecluster(cluster_id, node)
-        self._skip_logout = True
-        self.results['changed'] = True
-
-    def srx_cluster_disable(self):
-        """ Disable cluster mode on SRX device"""
-        logger.debug ('srx_cluster:disable cluster mode on srx device, rebooting')
-        self._tty.nc.disablecluster()
-        self._skip_logout = True
-        self.results['changed'] = True
-
     def zeroize(self):
         """ perform device ZEROIZE actions """
         logger.debug("zeroize : ZEROIZE device, rebooting")
@@ -268,140 +254,6 @@ class Console(object):
         for gather in FACT_LIST:
             gather(self, self._facts)
         self.results['facts'] = self._facts
-
-    """
-    def push_config(self, fname, action= 'merge'):
-        #push the configuration or rollback changes on error
-        if fname is not None and os.path.isfile(fname) is False:
-            self.results['failed'] = True
-            self.results[
-                'errmsg'] = 'ERROR: unknown file: {0}'.format(fname)
-            return self.results
-        logger.info("conf: loading into device....")
-        content = open(fname, 'r').read()
-        load_args = dict(content=content)
-        load_args['action'] = action  # merge/replace; yeah, I know ...
-        rc = self._tty.nc.load(**load_args)
-        if rc is not True:
-            self.results['failed'] = True
-            self.results['errmsg'] = 'failure to load configuration, aborting.'
-            logger.error('conf_ld_err: {0}'.format(self.results['errmsg']))
-            self._tty.nc.rollback()
-            return
-        logger.info('conf:  commit ... please be patient')
-        rc = self._tty.nc.commit()
-        if rc is not True:
-            self.results['failed'] = True
-            self.results[
-                'errmsg'] = 'faiure to commit configuration, aborting.'
-            logger.error('conf_save_err: {0}'.format(self.results['errmsg']))
-            self._tty.nc.rollback()
-            return
-        logger.info('conf,  commit completed')
-        self.results['changed'] = True
-        return
-    """
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # -------------------------------------------------------------------------
-    # QFX MODE processing
-    # -------------------------------------------------------------------------
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    def qfx_mode(self, mode):
-
-        # ----------------------------------------------------
-        # we need the facts, so if the caller didn't explicity
-        # request them, grab them now
-        # ----------------------------------------------------
-
-        if self._facts is None:
-            self._gather_facts()
-
-        # --------------------------------------------------------
-        # make sure we're logged into a QFX node device.
-        # set this up as a list check in case we have other models
-        # in the future to deal with.
-        # --------------------------------------------------------
-
-        if not any([self._facts['model'].startswith(m) for m in QFX_MODEL_LIST]):
-            self.results['errmsg'] = "Not on a QFX device [{0}]".format(
-                self._facts['model'])
-            self.results['failed'] = True
-            self.results['facts'] = self._facts
-            logger.error('qfx: {0}'.format((self.results['errmsg'])))
-            return
-
-        now, later = self._qfx_device_mode_get()
-        # compare to after-reoobt
-        change = bool(later != mode)
-        reboot = bool(now != mode)       # compare to now
-
-        if now == QFX_MODE_SWITCH and change is True:   # flipping to NODE
-            # --------------------------------------------------------
-            # we want to revert the facts information from the 'FPC 0'
-            # inventory, rather than the chassis, and re-save the facts
-            # --------------------------------------------------------
-            inv = self._tty.nc.facts.inventory
-            fpc0 = inv.xpath('chassis/chassis-module[name="FPC 0"]')[0]
-            self._facts['serialnumber'] = fpc0.findtext('serial-number')
-            self._facts['model'] = fpc0.findtext('model-number')
-        self.results['facts'] = self._facts
-        logger.debug("QFX mode now/later: {0}/{1}".format(now, later))
-        if now == later and later == mode:
-            # nothing to do
-            logger.debug('No change required')
-        else:
-            logger.debug('Action required')
-
-        if change is True:
-            logger.debug('Change: Changing the mode to: {0}'.format(mode))
-            self.results['changed'] = True
-            self._qfx_device_mode_set()
-
-        if reboot is True:
-            logger.debug('Change: REBOOTING device now!')
-            self.results['changed'] = True
-            self._tty.nc.reboot()
-            # no need to close the tty, since the device is rebooting ...
-            self._skip_logout = True
-
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # -------------------------------------------------------------------------
-    # MISC device RPC commands & controls
-    # -------------------------------------------------------------------------
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    # the following are the mode status string retrieved from NETCONF command
-    # to determine the current mode
-
-    _QFX_MODES = {
-        'Standalone': QFX_MODE_SWITCH,
-        'Node-device': QFX_MODE_NODE
-    }
-
-    # the following are the mode options inserted into the NETCONF command
-    # to change the mode
-
-    _QFX_XML_MODES = {
-        QFX_MODE_SWITCH: 'standalone',
-        QFX_MODE_NODE: 'node-device'
-    }
-
-    def _qfx_device_mode_get(self):
-        """ get the current device mode """
-        rpc = self._tty.nc.rpc
-        got = rpc('show-chassis-device-mode')
-        now = got.findtext('device-mode-current')
-        later = got.findtext('device-mode-after-reboot')
-        return (self._QFX_MODES[now], self._QFX_MODES[later])
-
-    def _qfx_device_mode_set(self, mode):
-        """ sets the device mode """
-        rpc = self._tty.nc.rpc
-        mode = self._QFX_XML_MODES[mode]
-        cmd = '<request-chassis-device-mode><{0}/></request-chassis-device-mode>'.format(
-            mode)
-        got = rpc(cmd)
-        return True
 
     # -----------------------------------------------------------------------
     # Context Manager
