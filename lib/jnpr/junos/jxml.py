137bb40452038fd0f42f8db03041f0cbbe8a39bc
Elisa Jasinska <elisa@packetfabric.com>
2016-08-23 14:56:40 +0000

    in rpc error: return None if ele.text is empty

--- lib/jnpr/junos/jxml.py
+++ lib/jnpr/junos/jxml.py
@@ -154,7 +154,7 @@ def rpc_error(rpc_xml):
 
     def find_strip(x):
         ele = rpc_xml.find(x)
-        return ele.text.strip() if None != ele else None
+        return ele.text.strip() if None != ele and None != ele.text else None
 
     this_err = {}
     this_err['severity'] = find_strip('error-severity')
