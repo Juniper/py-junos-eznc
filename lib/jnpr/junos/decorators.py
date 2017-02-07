58bd65671527e3ef5345b3caf9835925888f419c
vnitinv <nitin.nitp@gmail.com>
2017-02-07 18:10:24 +0530

    making code changes in new branch

--- lib/jnpr/junos/decorators.py
+++ lib/jnpr/junos/decorators.py
@@ -1,7 +1,9 @@
 # stdlib
 from functools import wraps
+import re
 
-from jnpr.junos.jxml import normalize_xslt
+from jnpr.junos.exception import RpcError
+from jnpr.junos import jxml as JXML
 
 
 def timeoutDecorator(function):
@@ -61,6 +63,49 @@ def normalizeDecorator(function):
                     except Exception:
                         dev.transform = restore_transform
                         raise
+            else:
+                try:
+                    return function(*args, **kwargs)
+                except Exception:
+                    raise
+        else:
+            try:
+                return function(*args, **kwargs)
+            except Exception:
+                raise
+
+    return wrapper
+
+
+def ignoreWarnDecorator(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        if 'ignore_warning' in kwargs or 'ignore_warning_message' in kwargs:
+            ignore_warn_msg = kwargs.pop('ignore_warning_message', None)
+            ignore_warn = kwargs.pop('ignore_warning', None) or \
+                          ignore_warn_msg is not None
+            if ignore_warn is True:
+                try:
+                    result = function(*args, **kwargs)
+                    return result
+                except RpcError as ex:
+                    ex.xml = JXML.remove_namespaces(ex.xml)
+                    if hasattr(ex, 'rpc_error') and\
+                                    ex.rpc_error['severity'] == 'warning':
+                        if ignore_warn_msg is None:
+                            return ex.xml
+                        elif isinstance(ignore_warn_msg, (str, unicode)):
+                            if re.search(ignore_warn_msg, ex.message, re.I):
+                                return ex.xml
+                        elif isinstance(ignore_warn_msg, list):
+                            for warn_msg in ignore_warn_msg:
+                                if re.search(warn_msg, ex.message, re.I):
+                                    return ex.xml
+                        raise ex
+                    else:
+                        raise ex
+                except Exception:
+                    raise
             else:
                 try:
                     return function(*args, **kwargs)
