eed6dd67f0eb9db9604da649791731d14f966ddf
Stacy Smith <stacywsmith@users.noreply.github.com>
2017-03-22 14:16:47 -0600

    setup script upgdated to note python 3.5 and 3.6 support.

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
