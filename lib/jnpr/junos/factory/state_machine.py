cac5cc407b524d9a5a8446cef6e2768498e352e6
vnitinv <nitin.nitp@gmail.com>
2017-09-11 16:43:56 +0530

    stable code before starting title cases

--- lib/jnpr/junos/factory/state_machine.py
+++ lib/jnpr/junos/factory/state_machine.py
@@ -5,7 +5,7 @@ import re
 import copy
 
 class Identifiers:
-    printables = pp.printables
+    printables = pp.OneOrMore(pp.printables)
     numbers = pp.Word(pp.nums)
     word = pp.Word(pp.alphanums) | pp.Word(pp.alphas)
     words = pp.OneOrMore(word)
@@ -13,12 +13,14 @@ class Identifiers:
     header_bar = pp.OneOrMore(pp.Word('-')) + pp.StringEnd()
 
 
-def is_integer(item):
+def data_type(item):
+    # should use identifiers class attribute
     try:
         Identifiers.numbers.parseString(item, parseAll=True)
+        return int
     except pp.ParseException as ex:
-        return False
-    return True
+        pass
+    return str
 
 
 class StateMachine(Machine):
@@ -34,14 +36,18 @@ class StateMachine(Machine):
         self.transitions = [
             {'trigger': 'column_provided', 'source': 'start', 'dest': 'row_column',
              'conditions': 'match_columns', 'before': 'check_header_bar',
-             'after': 'parse_raw_column'}, ]
+             'after': 'parse_raw_column'},
+            {'trigger': 'check_next_row', 'source': 'row_column', 'dest': 'row_column',
+             'conditions': 'prev_next_row_same_type',
+             'after': 'parse_raw_column'},
+        ]
         Machine.__init__(self, states=self.states, transitions=self.transitions,
                          initial='start', send_event=True)
 
     def parse(self, raw_data):
         self._data = {}
         self._lines = raw_data.splitlines()
-        if self._view.COLUMN is not None:
+        if len(self._view.COLUMN) > 0:
             self.column_provided()
         return self._data
 
@@ -75,42 +81,72 @@ class StateMachine(Machine):
 
     def parse_raw_column(self, event):
         col_offsets = {}
-        col_order = OrderedDict()
+        col_order = event.kwargs.get('col_order', OrderedDict())
         line = self._lines[0]
-        for key, column in self._view.COLUMN.items():
-            for result, start, end in pp.Literal(column).scanString(line):
-                col_offsets[(start, end)] = result[0]
-        user_defined_columns = copy.deepcopy(self._view.COLUMN)
-        for key in sorted(col_offsets.iterkeys()):
-            for x, y in self._view.COLUMN.items():
-                if col_offsets[key] == user_defined_columns.get(x):
-                    col_order[key] = x
-                    user_defined_columns.pop(x)
-                    break
+        if len(col_order) == 0:
+            for key, column in self._view.COLUMN.items():
+                for result, start, end in pp.Literal(column).scanString(line):
+                    col_offsets[(start, end)] = result[0]
+            user_defined_columns = copy.deepcopy(self._view.COLUMN)
+            for key in sorted(col_offsets.iterkeys()):
+                for x, y in self._view.COLUMN.items():
+                    if col_offsets[key] == user_defined_columns.get(x):
+                        col_order[key] = x
+                        user_defined_columns.pop(x)
+                        break
         # print col_order
-        key = self._table.KEY
-        if key not in self._view.COLUMN and key in self._view.COLUMN.values():
-            for user_provided, from_table in self._view.COLUMN.items():
-                if key == from_table:
-                    key = user_provided
+        key = self._get_key(event.kwargs.get('key', self._table.KEY))
         items = re.split('\s\s+', self._lines[1].strip())
 
-        post_integer_data_types = map(is_integer, items)
-        for line in self._lines[1:]:
+        post_integer_data_types = event.kwargs.get('check', map(data_type, items))
+        index = event.kwargs.get('index', 1)
+        col_len = len(col_order)
+        for index, line in enumerate(self._lines[index:], start=index):
             items = re.split('\s\s+', line.strip())
-            if len(items) == len(col_order):
+            if len(items) == col_len:
                 post_integer_data_types, pre_integer_data_types = \
-                    map(is_integer, items), post_integer_data_types
+                    map(data_type, items), post_integer_data_types
                 if post_integer_data_types == pre_integer_data_types:
-                    items = map(lambda x, y: int(x) if y is True else x,
+                    items = map(lambda x, y: int(x) if y is int else x,
                                 items, post_integer_data_types)
                     tmp_dict = dict(zip(col_order.values(), items))
-                    self._data[tmp_dict[key]] = tmp_dict
+                    if isinstance(key, tuple):
+                        self._data[tuple(tmp_dict[i] for i in key)] = tmp_dict
+                    else:
+                        self._data[tmp_dict[key]] = tmp_dict
                 else:
                     break
             elif line.strip() == '':
-                break
+                self.check_next_row(check=post_integer_data_types, data=self._data,
+                                    index=index, col_order=col_order,
+                                    key=key)
         return self._data
 
+    def _get_key(self, key):
+        if isinstance(key, list):
+            if set([i in self._view.COLUMN or i in self._view.COLUMN.values() for i in key]):
+                key_temp = []
+                for i in key:
+                    if i not in self._view.COLUMN and i in self._view.COLUMN.values():
+                        for user_provided, from_table in self._view.COLUMN.items():
+                            if i == from_table:
+                                key_temp.append(user_provided)
+                            else:
+                                key_temp.append(from_table)
+                    else:
+                        key_temp.append(i)
+                key = tuple(key_temp)
+        elif key not in self._view.COLUMN and key in self._view.COLUMN.values():
+            for user_provided, from_table in self._view.COLUMN.items():
+                if key == from_table:
+                    key = user_provided
+        return key
 
-
+    def prev_next_row_same_type(self, event):
+        index = event.kwargs.get('index')
+        post_integer_data_types = event.kwargs.get('check')
+        line = self._lines[index]
+        items = re.split('\s\s+', line.strip())
+        post_integer_data_types, pre_integer_data_types = \
+            map(data_type, items), post_integer_data_types
+        return post_integer_data_types == pre_integer_data_types
