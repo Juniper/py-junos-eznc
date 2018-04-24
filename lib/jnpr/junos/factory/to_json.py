2aace5534c7a2b57c434d1f49883d4cbf3494499
Nitin Kr <nitin.nitp@gmail.com>
2018-04-24 21:12:11 +0530

    to_json function support for CMDTable class

--- lib/jnpr/junos/factory/to_json.py
+++ lib/jnpr/junos/factory/to_json.py
@@ -11,11 +11,14 @@ class TableJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         from jnpr.junos.factory.view import View
         from jnpr.junos.factory.table import Table
+        from jnpr.junos.factory.cmdtable import CMDTable
 
         if isinstance(obj, View):
             obj = dict(obj.items())
         elif isinstance(obj, Table):
             obj = dict((str(item.name), item) for item in obj)
+        elif isinstance(obj, CMDTable):
+            obj = {str(key): val for key, val in obj.items()}
         else:
             obj = super(TableJSONEncoder, self).default(obj)
         return obj
