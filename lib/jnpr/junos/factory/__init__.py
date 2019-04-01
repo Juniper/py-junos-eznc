8ff7f5ff16caeb5e62651b98a6667814126c7b35
jjico <josh.jenkins@icann.org>
2019-04-01 15:30:41 -0700

    replace yaml with json; update factory appropriately

--- lib/jnpr/junos/factory/__init__.py
+++ lib/jnpr/junos/factory/__init__.py
@@ -1,33 +1,59 @@
+import glob
+import json
 import yaml
 import os.path
 
 from jnpr.junos.factory.factory_loader import FactoryLoader
 
-__all__ = ['loadyaml', 'FactoryLoader']
+__all__ = ['load', 'loadjson', 'loadyaml', 'FactoryLoader']
 
 
-def loadyaml(path):
+def load(path):
     """
-    Load a YAML file at :path: that contains Table and View definitions.
+    Load a file at :path: that contains Table and View definitions.
     Returns a <dict> of item-name anditem-class definition.
+    Supports both json and yaml natively; prefers JSON.
+    In this way additional support can be added (csv, xml, etc)
 
     If you want to import these definitions directly into your namespace,
     (like a module) you would do the following:
 
-      globals().update( loadyaml( <path-to-yaml-file> ))
+      globals().update( load( <path-to-file> ))
 
     If you did not want to do this, you can access the items as the <dict>.
-    For example, if your YAML file contained a Table called MyTable, then
+    For example, if your file contained a Table called MyTable, then
     you could do something like:
 
-      catalog = loadyaml( <path-to-yaml-file> )
+      catalog = load( <path-to-file> )
       MyTable = catalog['MyTable']
 
       table = MyTable(dev)
       table.get()
       ...
     """
+    ext = os.path.splitext(path)[1]
+    if ext == '':
+        # We don't have an ext, find it
+        candidates = glob.glob(path + '*')
+        for candy in candidates:
+            candy_ext = os.path.splitext(candy)[1]
+            if candy_ext == '.yml' or candy_ext = '.yaml':
+                return loadyaml(candy)
+            else:
+                return loadjson(candy)
+    elif ext == '.yml' or ext == '.yaml':
+        return loadyaml(path)
+    else:
+        return loadjson(path)
+
+def loadyaml(path):
     # if no extension is given, default to '.yml'
     if os.path.splitext(path)[1] == '':
         path += '.yml'
     return FactoryLoader().load(yaml.load(open(path, 'r')))
+
+def loadjson(path):
+    # if no extension is given, default to '.json'
+    if os.path.splitext(path)[1] == '':
+        path += '.json'
+    return FactoryLoader().load(json.load(open(path, 'r')))
--- lib/jnpr/junos/op/ethport.py
+++ lib/jnpr/junos/op/ethport.py
@@ -1,7 +1,7 @@
 """
 Pythonifier for EthPort Table/View
 """
-from jnpr.junos.factory import loadyaml
-from os.path import splitext
-_YAML_ = splitext(__file__)[0] + '.yml'
-globals().update(loadyaml(_YAML_))
+import jnpr.junos.factory
+import os
+_PATH_ = os.path.splitext(__file__)[0]
+globals().update(jnpr.junos.factory.load(_PATH_)
