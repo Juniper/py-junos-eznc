7fa3792df25da9deee77c59681015741e1548e3c
Stephen Steiner <ssteiner@juniper.net>
2020-07-10 14:59:35 -0400

    updated entrypoint to accept script and requirements.txt
    Signed-off-by: Stephen Steiner <ssteiner@juniper.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,55 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-    then /bin/bash 
+MSG="Valid arguments are one 'requirements.txt' file and/or one Python script"
 
-else 
-    echo $1
-    if [[ $1 == *"py" ]]
+if [  "${#@}" == 0 ]
+    then
+    echo "Starting an interactive bash session"
+    /bin/bash
+elif [ "${#@}" == 1 ]
     then 
-        /usr/bin/python3 "$1"
-    else
-        echo "Argument must be a Python file, ending in .py"
-        exit 1
-    fi  
+    case $1 in
+        requirements.txt)
+            echo "Installing Python packages defined in $1"
+            pip install -r "$1"
+            ;;
+        *.py)
+            echo "Executing $1 Python script"
+            /usr/bin/python3 "$1"
+            ;;
+        *)
+            echo "$MSG"
+            ;;
+    esac
+elif [ "${#@}" == 2 ]
+    then 
+    case $1 in
+        requirements.txt)
+            echo "Installing Python packages defined in $1"
+            pip install -r "$1"
+            ;;
+        *.py)
+            echo "Executing $1 Python script"
+            /usr/bin/python3 "$1"
+            ;;
+        *)
+            echo "$MSG"
+            ;;
+    esac
+    case $2 in
+        requirements.txt)
+            echo "Installing Python packages defined in $2"
+            pip install -r "$2"
+            ;;
+        *.py)
+            echo "Executing $2 Python script"
+            /usr/bin/python3 "$2"
+            ;;
+        *)
+            echo "$MSG"
+            ;;
+    esac
+elif [ "${#@}" -gt 2 ]
+    then
+    echo "Only 0, 1, or 2 arguments are expected. Got ${#@}"
 fi
