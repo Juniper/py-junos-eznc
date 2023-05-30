41974319abf03725b750fd7776bc59c557016e63
chidanandpujar <chidanandpujar@gmail.com>
2023-05-30 21:31:39 +0530

    Fix for ansible  issue 613 to support software installation on specific member_id

--- lib/jnpr/junos/utils/sw.py
+++ lib/jnpr/junos/utils/sw.py
@@ -1127,9 +1127,9 @@ class SW(Util):
             elif self._mixed_VC is True:
                 cmd.append(E("all-members"))
         elif (
-                self._multi_VC_nsync is True
-                or self._multi_VC is True
-                and member_id is not None
+            self._multi_VC_nsync is True
+            or self._multi_VC is True
+            and member_id is not None
         ):
             cmd.append(E("member", str(member_id)))
         if in_min >= 0 and at is None:
