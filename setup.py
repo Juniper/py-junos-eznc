4d2859faa97b99c4123edc1b9c391611cc700df8
dineshbaburam91 <dineshbaburam91@rediffmail.com>
2024-04-10 12:46:31 +0530

    Updated custom juniper paramiko

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
 import versioneer
+import pip
 
+def install(package):
+    if hasattr(pip, 'main'):
+        pip.main(['install', package])
+    else:
+        pip._internal.main(['install', package])
+
+# Install customer paramiko
+install("git+https://github.com/Juniper/paramiko.git")
 # parse requirements
 req_lines = [line.strip() for line in open("requirements.txt").readlines()]
 install_reqs = list(filter(None, req_lines))
