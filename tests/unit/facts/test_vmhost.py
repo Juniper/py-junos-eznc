0873df7a0162256b162d01fa090314f4fb24246d
Dinesh babu <dineshbaburam91@rediffmail.com>
2024-10-09 17:25:08 +0530

    Update test_vmhost.py

--- tests/unit/facts/test_vmhost.py
+++ tests/unit/facts/test_vmhost.py
@@ -1,7 +1,10 @@
 __author__ = "Stacy Smith"
 __credits__ = "Jeremy Schulman, Nitin Kumar"
 
-import unittest
+try:
+    import unittest2 as unittest
+except:
+    import unittest
 import nose2
 from unittest.mock import patch, MagicMock
 import os
