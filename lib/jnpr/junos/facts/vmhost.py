e1afeb517d9666075b04b1d3952bfaf129e276da
chidanandpujar <chidanandpujar@gmail.com>
2025-01-24 11:47:57 +0530

    Fix for issue #1355

--- lib/jnpr/junos/facts/vmhost.py
+++ lib/jnpr/junos/facts/vmhost.py
@@ -2,6 +2,32 @@ from jnpr.junos.exception import RpcError
 import re
 from lxml import etree
 
+def _get_vmhost_version_information(device):
+    multi_re = False
+    try:
+        rsp = device.rpc.get_route_engine_information(normalize=True)
+        re_list = rsp.findall(".//route-engine")
+        if len(re_list) > 1:
+            multi_re = True
+        else:
+            multi_re = False
+    except RpcError as err:
+        pass
+
+    if multi_re == True:
+        try:
+            return device.rpc.cli(
+                "show vmhost version invoke-on all-routing-engines", format="xml", normalize=True
+            )
+        except RpcError as err:
+            raise RpcError()
+    else:
+        try:
+            return device.rpc.cli(
+                "show vmhost version", format="xml", normalize=True
+            )
+        except RpcError as err:
+            raise RpcError()
 
 def provides_facts():
     """
@@ -19,6 +45,8 @@ def get_facts(device):
     """
     SYSCTL_VMHOST_MODE = "sysctl -n hw.re.vmhost_mode"
     vmhost = None
+    vmhost_info = {}
+    vm_ver_rsp = None
 
     if device.facts["_is_linux"]:
         vmhost = False
@@ -35,6 +63,30 @@ def get_facts(device):
         except RpcError:
             pass
 
+    if vmhost == True:
+        rsp = _get_vmhost_version_information(device)
+        if rsp.tag == "output":
+            pass
+        else:
+            if rsp.tag == "vmhost-version-information":
+                vm_ver_rsp = [rsp]
+            else:
+                vm_ver_rsp = rsp.findall(".//vmhost-version-information")
+            vmhost_info = {}
+
+            for re_vm_ver_info in vm_ver_rsp:
+                re_name = re_vm_ver_info.findtext("../re-name", "re0")
+                vmhost_current_root_set = re_vm_ver_info.findtext("./current-root-set")
+                vmhost_set_junos_version_set_p = re_vm_ver_info.findtext("./set-disk-info[set-disk-name = 'set p']/set-junos-version")
+                vmhost_set_junos_version_set_b = re_vm_ver_info.findtext("./set-disk-info[set-disk-name = 'set b']/set-junos-version")
+
+                vmhost_info[re_name] = {
+                    "vmhost_current_root_set": vmhost_current_root_set,
+                    "vmhost_version_set_p" : vmhost_set_junos_version_set_p,
+                    "vmhost_version_set_b": vmhost_set_junos_version_set_b,
+               }
+
     return {
         "vmhost": vmhost,
+        "vmhost_info": vmhost_info,
     }
